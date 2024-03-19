83885957c474bef620f55e1da0d655a1b2904c7e
Pavel Larchenko <pl@napoleonit.ru>
2024-03-19 18:20:01 +0500

    📝 add swagger docs for ProductRetrieveAPIView

--- backend/products/views/products_crud.py
+++ backend/products/views/products_crud.py
@@ -1,3 +1,5 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -10,6 +12,13 @@ class ProductRetrieveAPIView(GenericAPIView):
     serializer_class = ProductsOutputSerializer
     lookup_url_kwarg = 'product_pk'
 
+    @extend_schema(
+        tags=['products'],
+        summary='Получить один продукт',
+        operation_id='product_retrieve',
+        responses={status.HTTP_201_CREATED: ProductsOutputSerializer},
+        auth=[],
+    )
     def get(self, request: Request, product_pk: int):
         product = get_object_or_404(Products, pk=product_pk)
         self.check_object_permissions(request, product)
