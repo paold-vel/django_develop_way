4653bb729bec3ac6c64c5187ed3f3d613051b1e2
Pavel Larchenko <pl@napoleonit.ru>
2024-03-19 18:19:29 +0500

    📝 add swagger route

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(api_version='api/1.0'), name='schema'),
+    path(
+        'docs/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
+
     path('admin/', admin.site.urls),
 
     path('health/', include('backend.health.urls')),
